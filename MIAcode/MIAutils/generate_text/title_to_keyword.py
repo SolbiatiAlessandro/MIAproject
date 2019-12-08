9be34e6faa54fab38f5b7c35e18e137d0a46e8ce
Oana Florescu <oana.florescu28@gmail.com>
2019-12-08 02:56:00 +0000

    create .txt files with scripts in spanish given yt videos titles

---
+++ MIAcode/MIAutils/generate_text/title_to_keyword.py
@@ -0,0 +1,23 @@
+import re
+from typing import Optional
+
+def title_to_keyword(input_yt_title: str) -> Optional[str]:
+    #input_yt_title
+    re_match_pattern1 = re.search('(?<=is )(.*)(?=\? What)', input_yt_title)
+    re_match_pattern2 = re.search('(?<=does )(.*)(?= mean\?)', input_yt_title)
+    
+    if re_match_pattern1:
+        re_match = re_match_pattern1
+    elif re_match_pattern2:
+        re_match = re_match_pattern2
+    else:
+        return
+
+    keyword = re_match.group(0)
+    no_spaces_keyword = keyword.replace(' ', '_')
+    return no_spaces_keyword
+
+if __name__ == '__main__':
+    example = "What does CRINGE mean? CRINGE definition - CRINGE meaning - How to pronounce CRINGE"
+    example2 = "What is HUMIDITY? What does HUMIDITY mean? HUMIDITY meaning - How to pronounce HUMIDITY?"
+    print(title_to_keyword(example))
